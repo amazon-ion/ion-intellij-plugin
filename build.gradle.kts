034dec577ad37b7b350fd486774176e8b36117b2
Matthew Pope <81593196+popematt@users.noreply.github.com>
2024-01-02 11:39:40 -0800

    Release v2.5.0 (#60)

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,7 +77,7 @@ val pluginGroup: String by project
 // `pluginName_` variable ends with `_` because of the collision with Kotlin magic getter in the `intellij` closure.
 // Read more about the issue: https://github.com/JetBrains/intellij-platform-plugin-template/issues/29
 val pluginName_: String by project
-val pluginVersion: String = pluginVersion(major = "2", minor = "4", patch = "0")
+val pluginVersion: String = pluginVersion(major = "2", minor = "5", patch = "0")
 val pluginDescriptionFile: String by project
 val pluginChangeNotesFile: String by project
 
